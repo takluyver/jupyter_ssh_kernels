8293cf1704e43b01c8ba5cb5b7f3a8b21b7ef8b7
Thomas Kluyver <thomas@kluyver.me.uk>
2018-01-29 16:47:21 +0000

    Fixes in remote code

--- remote_launch.py
+++ remote_launch.py
@@ -1,6 +1,6 @@
 """Sent to Python on the remote system to launch a kernel."""
 
 from jupyter_client.manager2 import KernelManager2
-km = KernelManager2(kernel_cmd={argv}, cwd={cwd}, ip={address})
-print("!!!Started, connection_file:", km.connection_file, '!!!')
-km.wait()
+km = KernelManager2(kernel_cmd={argv!r}, cwd={cwd!r}, ip={address!r})
+print("!!!Started, connection_file:", km.connection_file, '!!!', flush=True)
+km.wait(None)
