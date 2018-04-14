6852012b52e46cedcabe7a5a1c9930196b313198
Thomas Kluyver <thomas@kluyver.me.uk>
2018-04-14 20:14:34 +0200

    Add code execution to example

--- jupyter_ssh_kernels/__main__.py
+++ jupyter_ssh_kernels/__main__.py
@@ -2,7 +2,7 @@ from jupyter_kernel_mgmt.client import BlockingKernelClient
 from .provider import SSHKernelProvider
 
 # Try starting a remote kernel and connecting to it.
-conn_info, km = SSHKernelProvider().launch('mydesktop')
+conn_info, km = SSHKernelProvider().launch('local')
 print("Started remote kernel")
 print()
 print(conn_info)
@@ -10,11 +10,25 @@ print()
 
 kc = BlockingKernelClient(conn_info, km)
 print("Getting kernel info...")
-print(kc.kernel_info(reply=True).content)
+print(kc.kernel_info().content)
 print()
 
+print("Executing some code...")
+kc.execute_interactive("""
 import time
-time.sleep(5)
-print("Shutting down...")
+
+for a in range(6):
+    print(a)
+    time.sleep(1)
+
+input("Press enter to finish> ")
+print("End of submitted code")
+""")
+print("Finished executing")
+
+import time
+time.sleep(2)
+print("\nShutting down...")
 kc.shutdown_or_terminate()
+kc.close()
 print("Shutdown complete")
